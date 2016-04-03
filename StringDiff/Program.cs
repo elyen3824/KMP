92b0d49662078bff1be855b4e8014660f30608e5
elyen3824 <julien.pohie@gmail.com>
2016-04-03 23:45:28 +0100

    initial commit

---
+++ StringDiff/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StringDiff
+{
+    public class KMPplus
+    {
+        private int[] next;
+        private string pattern;
+        private int pattern_length;
+
+        public KMPplus(string pattern)
+        {
+            this.pattern = pattern;
+            this.pattern_length = pattern.Length;
+            this.next = new int[pattern_length];
+            int j = -1;
+            for (int i = 0; i < pattern_length; i++)
+            {
+                if (i == 0)
+                    next[i] = -1;
+                else if (pattern[i] != pattern[j])
+                    next[i] = j;
+                else
+                    next[i] = next[j];
+                while (j >=0 && pattern[i] != pattern[j])
+                {
+                    j = next[j];
+                }
+                j++;
+            }
+        }
+
+        public int search(string str)
+        {
+            int length = str.Length;
+            int i, j;
+            for (i = 0,j=0; i < length && j<pattern_length; i++)
+            {
+                while (j >= 0 && str[i] != pattern[j])
+                    j = next[j];
+                j++;
+            }
+            if (j == pattern_length)
+                return i - pattern_length;
+            return -1;
+        }
+    }
+}
---
+++ UnitTestProject1/StringDiffTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using StringDiff;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class StringDiffTest
+    {
+        [Test]
+        public void Should_Find_Pattern_In_String()
+        {
+            string str = "BCBAABACAABABACAA";
+            string pattern = "ABABAC";
+
+            var kmpPlus = new KMPplus(pattern);
+            var result = kmpPlus.search(str);
+
+            Assert.AreEqual(9,result);
+        }
+
+        [Test]
+        public void Should_Not_Find_Pattern_In_String()
+        {
+            string str = "BCBAABACAABABACAA";
+            string pattern = "WXYZ";
+
+            var kmpPlus = new KMPplus(pattern);
+            var result = kmpPlus.search(str);
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
